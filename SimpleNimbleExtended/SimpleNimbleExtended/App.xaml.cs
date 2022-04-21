aa0c6a8b6e1e556927e99d2e438e95ddebf9411d
Florian <Florianlebecque@hotmail.com>
2022-04-21 13:04:39 +0200

    Now load Controler and API

--- SimpleNimbleExtended/SimpleNimbleExtended/App.xaml.cs
+++ SimpleNimbleExtended/SimpleNimbleExtended/App.xaml.cs
@@ -2,13 +2,27 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using SimpleNimbleExtended.API;
+using SimpleNimbleExtended.API.Implementation;
+using SimpleNimbleExtended;
+using System.Resources;
+
 namespace SimpleNimbleExtended
 {
     public partial class App : Application {
         public App() {
             InitializeComponent();
 
+            
+
+            ISNapi simpleNimbleAPI = new SimpleNimbleAPI(SimpleNimbleExtended.Properties.AppResources.API_URL);
+
+            Controler ctrl = Controler.GetInstance();
+            ctrl.Load(simpleNimbleAPI);
+
+            
             MainPage = new NavigationPage( new MainPage());
+            
         }
 
         protected override void OnStart() {
