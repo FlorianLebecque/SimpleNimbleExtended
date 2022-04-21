271da75ca63a3cb016786b65e5246b28c2eda3c7
Florian <Florianlebecque@hotmail.com>
2022-04-21 13:04:57 +0200

    Added register

--- SimpleNimbleExtended/SimpleNimbleExtended/LoginRegister.xaml.cs
+++ SimpleNimbleExtended/SimpleNimbleExtended/LoginRegister.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SimpleNimbleExtended;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,6 +14,9 @@ namespace SimpleNimbleExtended
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginRegister : ContentPage {
         public LoginRegister() {
+            NavigationPage.SetHasNavigationBar(this, false);
+            NavigationPage.SetHasBackButton(this, false);
+
             InitializeComponent();
         }
 
@@ -66,5 +71,39 @@ namespace SimpleNimbleExtended
             isRegisterExpend = !isRegisterExpend;
         }
         #endregion
+
+        private void Bt_register_Clicked(object sender, EventArgs e) {
+            Controler ctrl = Controler.GetInstance();
+
+            string username = tb_userRegister.Text;
+            string email = tb_emailRegister.Text;
+            string passw1 = tb_pass1Register.Text;
+            string passw2 = tb_pass2Register.Text;
+
+            if ((passw1 == passw2)&&(ctrl.Register(username,email, passw1,sw_KeepRegister.IsToggled)))
+            {
+                Navigation.PushAsync(new Main());
+            }
+            else
+            {
+                DisplayAlert("Register", "Make sure your email is valide and your password match", "OK");
+            }
+        }
+
+        private void Bt_login_Clicked(object sender, EventArgs e) {
+
+    
+            Controler ctrl = Controler.GetInstance();
+
+            string username = tb_userLogin.Text;
+            string passw = tb_passLogin.Text;
+
+            if (ctrl.Login(username, passw,sw_KeepLogin.IsToggled)) {
+                Navigation.PushAsync(new Main());
+            } else {
+                DisplayAlert("Login", "Email or password incorrect","OK");
+            }
+
+        }
     }
 }
