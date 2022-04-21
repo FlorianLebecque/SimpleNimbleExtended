a47ebbee1d7139a84f0ee8c696315417244a3e67
Florian <Florianlebecque@hotmail.com>
2022-04-21 13:05:20 +0200

    Display connected account and change tab title

---
+++ SimpleNimbleExtended/SimpleNimbleExtended/Main.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleNimbleExtended {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Main : TabbedPage {
+        public Main() {
+            InitializeComponent();
+
+            NavigationPage.SetHasBackButton(this,false);
+
+            Label lb = new Label();
+            lb.Text = Controler.GetInstance().username;
+            lb.FontSize = Device.GetNamedSize(NamedSize.Title, typeof(Label));
+            lb.TextColor = Color.White;
+
+            NavigationPage.SetTitleView(this,lb);
+            
+        }
+    }
+}
