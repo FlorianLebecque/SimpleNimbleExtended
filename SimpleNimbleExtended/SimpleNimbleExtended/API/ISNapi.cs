cabb1ba9e8adadab87020179738b5dd11da87f72
Florian Lebecque <=>
2022-04-28 22:18:03 +0200

    Added find user, New post, get post, get user info, follow unfollow , get followed User

--- SimpleNimbleExtended/SimpleNimbleExtended/API/ISNapi.cs
+++ SimpleNimbleExtended/SimpleNimbleExtended/API/ISNapi.cs
@@ -9,5 +9,18 @@ namespace SimpleNimbleExtended.API {
 
         dynamic Register(string username,string email, string password);
 
+        dynamic FindUsers(string username);
+
+        dynamic AddNewPost(string username, string token, string title, string content, string imgUrl);
+
+        dynamic GetPostOfUser(string id);
+
+        dynamic GetUserInfo(string id);
+
+        dynamic Follow(string username, string token,string id);
+
+        dynamic UnFollow(string username, string token,string id);
+
+        dynamic GetFollowedUser(string username, string token);
     }
 }
